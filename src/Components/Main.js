8d8a2792c4e8a0eeb1981596ad8d7b1e6441c214
Asset <janotyli98@gmail.com>
2022-12-01 14:18:13 +0600

    reactStarWars

---
+++ src/Components/Main.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { Card } from "./Card"
+import axios from "axios"
+import { useState } from "react"
+import { useEffect } from "react"
+export const Main = () => {
+  const [url,setUrl]=useState("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=2e1cdeec426ae323484f29024084c206&hash=d516513ba95b9407c7aca0f73b241f8a")
+  const [item,setItem]=useState();
+  const [search,setSearch]=useState("");
+  useEffect(()=>{
+    const fetch=async()=>{
+      const res=await axios.get(url)
+      setItem(res.data.data.results);
+    }
+    fetch();
+  },[url])
+  
+  const searchMarvel=()=>{
+    setUrl(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${search}&ts=a767441d3b5eeb8a6776f991321a7cf0&hash=1b7d3caf023c7a8f73a709d19410d87b`)
+  }
+
+  return (
+    <>
+        <div className="header">
+            <div className="bg">
+                <img src="./img/m1.jpg" alt="" />
+            </div>
+            <div className="search-bar">
+                <img src="./img/m2.png" alt="logo" className="search-img"/>
+                <input type="search" placeholder='Search Here'
+                 className='search'
+                 onChange={e=>setSearch(e.target.value)}
+                 onKeyPress={searchMarvel}/>
+            </div>
+        </div>
+       <div className="content">
+         
+        {
+          (!item)?<p>Not Found</p>:<Card data={item}/>
+        }
+       </div>
+    </>
+  )
+}
